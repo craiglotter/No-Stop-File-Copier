b2d2f174c66c7693c681d2d32afc1a0b8b6e4a11
Craig Lotter <craig.lotter@gmail.com>
2014-08-13 22:32:34 +0200

    populate repo

---
+++ Source Code/No Stop File Copier/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("No Stop File Copier")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("CodeUnit")> 
+<Assembly: AssemblyProduct("No Stop File Copier")> 
+<Assembly: AssemblyCopyright("Copyright © CodeUnit 2006")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("9e7762f7-293d-4805-9f5d-3a26bffbbdda")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("2006.07.11.1")> 
+<Assembly: AssemblyFileVersion("2006.07.11.1")> 
